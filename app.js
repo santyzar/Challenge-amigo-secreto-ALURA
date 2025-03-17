8a99619312045767eb2aee96a4db01168a87d7df
santyzar <137662721+santyzar@users.noreply.github.com>
2025-03-17 17:17:12 -0300

    1. Capture el valor del campo de entrada: Utilizando `document.getElementById` para obtener el texto ingresado por el usuario. 2. Valido la entrada: Implementando una validación para asegurarse de que el campo no esté vacío. Si está vacío, muestra un `alert` con el mensaje de error: "Por favor, inserte un nombre." 3. Actualizo el array de amigos: Si el valor es válido, se añáde al arreglo que almacena los nombres de amigos usando el método `.push()`. 4. Limpio el campo de entrada: Después de añadir el nombre, restablezco el campo de texto a una cadena vacía.

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
 let amigos = [];
+
+function agregarAmigo(){
+    let input = document.getElementById("amigo");
+    let nombre = input.ariaValueMax.trim();
+    
+    if (nombre === ""){
+        alert("Por favor, inserte un nombre.");
+        return;
+    }
+    amigos.push(nombre);
+    actualizarLista();
+    input.value = "";
+}
