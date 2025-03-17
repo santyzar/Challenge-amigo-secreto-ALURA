67183ba4c2dda7b0af1ee23e9c8baec193b81ba2
santyzar <137662721+santyzar@users.noreply.github.com>
2025-03-17 19:42:09 -0300

    corregir errores

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ let amigos = [];
 
 function agregarAmigo(){
     let input = document.getElementById("amigo");
-    let nombre = input.ariaValueMax.trim();
+    let nombre = input.value.trim();
     
     if (nombre === ""){
         alert("Por favor, inserte un nombre.");
