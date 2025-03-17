402c3b22823e4f20eb240c96bbb7682a8d90a6cb
santyzar <137662721+santyzar@users.noreply.github.com>
2025-03-17 18:46:47 -0300

    Implemento una función para actualizar la lista de amigos. Seleccioné el elemento de la lista** usando `document.getElementById`  para mostrar los amigos. Limpié la lista existente estableciendo `lista.innerHTML = ""` para evitar duplicados al actualizar. Recorrí el arreglo `amigos` usando un bucle `for` y creé elementos de lista (`<li>`) para cada nombre. Agregué elementos a la lista creando un nuevo elemento de lista (`<li>`) para cada amigo.

--- app.js
+++ app.js
@@ -12,3 +12,12 @@ function agregarAmigo(){
     actualizarLista();
     input.value = "";
 }
+function actualizarLista(){
+    let lista = document.getElementById("listaAmigos");
+    lista.innerHTML = "";
+    for (let i = 0; i < amigos.length; i++){
+        let li = document.createElement("li");
+        li.textContent = amigos[i];
+        lista.appendChild(li);
+    }
+}
